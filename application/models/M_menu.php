cd8032ece6e4822d04a05718d2f430e4330bfdfc
sayaphujan <shinta.setiawati@gmail.com>
2019-06-17 17:13:36 +0700

    first commit

--- application/models/M_menu.php
+++ application/models/M_menu.php
@@ -33,48 +33,4 @@ class M_menu extends CI_Model {
 
         return $res;
     }
-
-    function get_saran($min, $max){
-        $res = $this->db->query("select DATE(tgl_complain) as tgl_saran, COUNT('*') as saran from tb_complain WHERE jenis_complain='saran' AND DATE(tgl_complain) BETWEEN '".$min."' AND '".$max."' GROUP BY DATE(tgl_complain)");
-        //return array_column($res->result(), 'saran');
-        if($res->num_rows() > 0){
-            foreach($res->result() as $data){
-                $hasil[] = $data;
-            }
-            return $hasil;
-        }
-    }
-
-    function get_kritik($min, $max){
-        $res = $this->db->query("select DATE(tgl_complain) as tgl_kritik, COUNT('*') as kritik from tb_complain WHERE jenis_complain='kritik' AND DATE(tgl_complain) BETWEEN '".$min."' AND '".$max."' GROUP BY DATE(tgl_complain)");
-        //return array_column($res->result(), 'kritik');
-        if($res->num_rows() > 0){
-            foreach($res->result() as $data){
-                $hasil[] = $data;
-            }
-            return $hasil;
-        }
-    }
-
-    function get_category($min, $max){
-        $res = $this->db->query("select DATE(tgl_complain) as tanggal from tb_complain WHERE DATE(tgl_complain) BETWEEN '".$min."' AND '".$max."' GROUP BY DATE(tgl_complain)");
-        //return array_column($res->result(), 'jenis');
-        if($res->num_rows() > 0){
-            foreach($res->result() as $data){
-                $hasil[] = $data;
-            }
-            return $hasil;
-        }
-    }
-
-    function report($min, $max){
-        $query = $this->db->query("select jenis_complain as jenis, count('*') as jumlah from tb_complain WHERE DATE(tgl_complain) BETWEEN '2019/06/01' AND '2019/06/15' GROUP BY jenis_complain");
-         
-        if($query->num_rows() > 0){
-            foreach($query->result() as $data){
-                $hasil[] = $data;
-            }
-            return $hasil;
-        }
-    }
 }
