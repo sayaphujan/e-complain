f0425a32b8572f70d97398ca879edd5b48faee99
Shinta Setiawati <shinta.setiawati@gmail.com>
2019-06-17 17:21:12 +0700

    first commit
    first commit

--- application/controllers/Diagram.php
+++ application/controllers/Diagram.php
@@ -9,24 +9,6 @@ class Diagram extends CI_Controller {
     }
     
 	public function index() {
-		$min = $this->input->post('min-date');
-		$max = $this->input->post('max-date');
-
-		if($min==''){
-			//$min = date('Y-m-d', strtotime("-30 days"));
-			$min = date('Y/m/01');
-			$data['min_date'] = $min;
-		}else{
-			$data['min_date'] = $this->input->post('min-date');
-		}
-
-		if($max==''){
-			$max = date('Y/m/d');
-			$data['max_date'] = $max;
-		}else{
-			$data['max_date'] = $this->input->post('max-date');
-		}
-
 		/*** PIE PAYMENT ***/
 		$array_kategori = array('Jumlah Data');
 		$array_series = array(array('name'=>'Jumlah Data', 'data'=>array()));
@@ -48,10 +30,6 @@ class Diagram extends CI_Controller {
 		$data['pie'] = json_encode($array_series);
 		/*** END ***/
 
-		$data['chart_saran'] = $this->M_menu->get_saran($min, $max);
-		$data['chart_kritik'] = $this->M_menu->get_kritik($min, $max);
-		$data['category'] = $this->M_menu->get_category($min, $max);
-		$data['report'] = $this->M_menu->report($min, $max);
 		$data['total'] = $this->M_menu->total($min, $max);
 
         $this->template->display('backend/template','diagram', $data);   
